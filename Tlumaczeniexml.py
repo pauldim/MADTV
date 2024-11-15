851758c2d690c69940f0ab280c08cdbe8ef0d327
pauldim <133207976+pauldim@users.noreply.github.com>
2024-11-15 14:44:00 +0100

    Update Tlumaczeniexml.py

--- Tlumaczeniexml.py
+++ Tlumaczeniexml.py
@@ -18,10 +18,13 @@ def add_polish_translation_to_xml(file_path):
             # Dodawanie <pl> za <en>
             parent = en_elem.getparent()
             parent.insert(parent.index(en_elem) + 1, pl_elem)
+            
+            # Dodawanie końca linii po elemencie <pl>
+            pl_elem.tail = "\n"
 
     # Zapisywanie zmodyfikowanego pliku
     output_path = "C:/xampp/htdocs/MADTV/translated_output.xml"
-    tree.write(output_path, encoding="UTF-8", xml_declaration=True)
+    tree.write(output_path, encoding="UTF-8", xml_declaration=True, pretty_print=True)
     print(f"Tłumaczenie dodane do pliku '{output_path}'.")
 
 add_polish_translation_to_xml("C:/xampp/htdocs/MADTV/database_scripts.xml")
